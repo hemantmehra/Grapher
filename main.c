3afcbf6c2577d2c702288fe89b7907e5fc4c1595
Hemant Mehra <mehra.hemant14@gmail.com>
2024-11-26 23:55:00 +0530

    add ast and eval_ast

--- main.c
+++ main.c
@@ -1,11 +1,43 @@
 #include <math.h>
 #include <stdio.h>
+#include <assert.h>
 #include <raylib.h>
 
 int screen_width = 1600;
 int screen_height = 900;
 
 #define SAMPLE_SIZE 4000
+
+typedef enum {
+    PLUS,
+    MUL,
+    SIN,
+    COS
+} Operator;
+
+typedef enum {
+    Binary,
+    Symbol,
+    Number
+} Kind;
+
+struct _ast {
+    Kind kind;
+
+    union
+    {
+        struct
+        {
+            Operator op;
+            struct _ast *left;
+            struct _ast *right;
+        } binary;
+        double number;
+        char symbol;
+    } main;
+};
+
+typedef struct _ast Ast;
 
 double translate_x(double x, double scale)
 {
@@ -39,12 +71,61 @@ void plot_points(Vector2 *points, double step, double scale)
     }
 }
 
+double eval_ast(Ast *ast, double x)
+{
+    switch (ast->kind)
+    {
+    case Symbol:
+        return x;
+    
+    case Binary:
+        {
+            switch(ast->main.binary.op) {
+                case PLUS:
+                {
+                    double l = eval_ast(ast->main.binary.left, x);
+                    double r = eval_ast(ast->main.binary.right, x);
+                    return l + r;
+                }
+
+                default:
+                    assert(false);
+                    break;
+            }
+            break;
+        }
+    
+    case Number:
+        return ast->main.number;
+
+    default:
+        assert(false);
+        break;
+    }
+}
+
 int main(void)
 {
     InitWindow(screen_width, screen_height, "Grapher");
     double scale = 50;
     double step = 0.01;
     Vector2 points[SAMPLE_SIZE];
+
+    Ast ast_x;
+    ast_x.kind = Symbol;
+    ast_x.main.symbol = 'x';
+
+    Ast ast_4;
+    ast_4.kind = Number;
+    ast_4.main.number = 4;
+
+    Ast ast;
+    ast.kind = Binary;
+    ast.main.binary.left = &ast_x;
+    ast.main.binary.right = &ast_4;
+
+    double r = eval_ast(&ast, 5);
+    printf("[r=%lf]\n", r);
 
     plot_points(points, step, scale);
     // for (int i = 0; i < SAMPLE_SIZE; i++) {
